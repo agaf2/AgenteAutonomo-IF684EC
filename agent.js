54ca820efd5380eabde09237f23cb30b79d4187a
agaf2 <agaf2@cin.ufpe.br>
2024-06-02 12:05:33 -0300

    wip: food and agent

---
+++ agent.js
@@ -0,0 +1,17 @@
+class Agent{
+    constructor(graph, x, y ){
+        this.x = x;
+        this.y = y;
+        this.speed = 0;
+        this.graph = graph;
+        this.path = [];
+        this.current = 0;
+        this.finished = false;
+    }
+
+    seek(food){
+        
+    }
+
+
+}
---
+++ food.js
@@ -0,0 +1,12 @@
+class Food{
+    constructor(x, y, imgPath){
+        this.x = x;
+        this.y = y;
+        this.img = loadImage(imgPath);
+    }
+
+    display() {
+        image(this.img, this.x, this.y);
+    }
+    
+}
